caff9731b7b19e6dc435405a4e21a113b318452c
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2022-04-24 21:42:10 -0400

    UI added along with re-factored JS. Explanation added to README.md. Test JS files removed

--- js/app.js
+++ js/app.js
@@ -1,175 +1,197 @@
-// ******************************* //
-
-// Write computerPlay() function. Randomly returns
-// "Rock", "Paper", or "Scissors" for the computer
 function computerPlay() {
-  // Array for computer selections
-  // const selection = ["Rock", "Paper", "Scissors"];
-  const selection = ["Rock", "Rock", "Rock"];
+  const selection = ["rock", "paper", "scissors"];
+  // const selection = ["rock", "rock", "rock"];
   const len = selection.length;
   let choice = Math.floor(Math.random() * len);
+  // console.log(`Random num: ${choice}`);
+  // console.log(`Selection: ${selection[choice]}`);
   return selection[choice];
 }
-
-// Player selection
+// computerPlay();
+// let playerSelection = prompt("Enter a selection");
 let playerSelection;
-let computerSelection = computerPlay();
+// let computerSelection = computerPlay();
 
-// Counters
-let gameCounter = 0;
-let playerCount = 0;
-let computerCount = 0;
-let gamesDrawnCounter = 0;
-
-////////////////////////////////////////
-//Rock, Paper, Scissors 23 de abril, 2022 //
-////////////////////////////////////////
-//  ********* UI ***************
-// Display Container
-const resultsDisplay = document.querySelector(".score-display");
-// Game Buttons
-const gameBtns = document.querySelectorAll(".choiceBtn");
+// ****** UI ******* //
+// Buttons ******** //
+const chooseBtn = document.querySelectorAll(".choiceBtn");
 const refreshBtn = document.querySelector(".refreshBtn");
-gameBtns.forEach(item => {
+
+// Scores ******** //
+const playerScore = document.querySelector(".player-score");
+const computerScore = document.querySelector(".computer-score");
+
+// Main Score Display ******* //
+const mainScoreDisplay = document.querySelector(
+  ".main-score-display-container"
+);
+
+chooseBtn.forEach(item => {
   item.addEventListener("click", e => {
-    playerSelection = e.currentTarget.dataset.id;
-    console.log(playerSelection);
+    let playerSelection = e.currentTarget.dataset.id;
+    // console.log(playerSelection);
     game(playerSelection, computerPlay());
+    // displayGame(playerSelection, computerPlay(), gameCount, gamesTied);
   });
 });
-// Start new game
-refreshBtn.addEventListener("click", () => {
-  console.log("Refreshing...");
-  game(playerSelection, computerPlay());
-});
-// Write playRound() function that takes
-// 2 parameters(playerSelection, computerSelection)
-// And displays a winner
+
+// Counters
+let playerCount = 0;
+let computerCount = 0;
+let gameCount = 0;
+let gamesTied = 0;
+
 function playRound(playerSelection, computerSelection) {
-  // Convert to lowercase for comparison
-  const playerChoice = playerSelection.toLowerCase();
-  const computerChoice = computerPlay().toLowerCase();
+  // Player Selection
+  if (playerSelection === computerSelection) {
+    gamesTied++;
+    gameCount++;
+    console.log("You chose the same. Try again!");
+    console.log(
+      `Player says: ${playerSelection}. Computer says: ${computerSelection}`
+    );
+    console.log(`Games tied: ${gamesTied}`);
+    mainScoreDisplay.innerHTML = `   <p class="main-score-round-winner">
+            You chose the same<br />
+            Try again!
+          </p>
+          <p class="main-score-display">Games Played: ${gameCount}</p>
+          <p class="main-score-display">Games tied: ${gamesTied}</p>`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log(
+      `Player says: ${playerSelection}. Computer says: ${computerSelection}`
+    );
+    console.log(
+      `You win this round. ${playerSelection} beats ${computerSelection}`
+    );
+    gameCount++;
+    playerCount++;
 
-  // Check for same choice
-  if (playerChoice === computerChoice) {
-    gamesDrawnCounter++;
-    console.log("Same choice. Try again!");
-  } else if (playerChoice === "rock" && computerChoice === "scissors") {
-    playerRoundWinner(playerChoice, computerChoice);
+    mainScoreDisplay.innerHTML = `   <p class="main-score-round-winner">
+          You win this round! <br />
+          ${playerSelection} beats ${computerSelection}
+        </p>
+        <p class="main-score-display">Games played: ${gameCount}</p>
+        <p class="main-score-display">Games tied: ${gamesTied}</p>`;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log(
+      `Player says: ${playerSelection}. Computer says: ${computerSelection}`
+    );
+    console.log(
+      `You win this round. ${playerSelection} beats ${computerSelection}`
+    );
+    gameCount++;
     playerCount++;
-  } else if (playerChoice === "scissors" && computerChoice === "paper") {
-    playerRoundWinner(playerChoice, computerChoice);
+    mainScoreDisplay.innerHTML = `   <p class="main-score-round-winner">
+          You win this round! <br />
+          ${playerSelection} beats ${computerSelection}
+        </p>
+        <p class="main-score-display">Games played: ${gameCount}</p>
+        <p class="main-score-display">Games tied: ${gamesTied}</p>`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log(
+      `Player says: ${playerSelection}. Computer says: ${computerSelection}`
+    );
+    console.log(
+      `You win this round. ${playerSelection} beats ${computerSelection}`
+    );
+    gameCount++;
     playerCount++;
-  } else if (playerChoice === "paper" && computerChoice === "rock") {
-    playerRoundWinner(playerChoice, computerChoice);
-    playerCount++;
+    mainScoreDisplay.innerHTML = `   <p class="main-score-round-winner">
+          You win this round! <br />
+          ${playerSelection} beats ${computerSelection}
+        </p>
+        <p class="main-score-display">Games played: ${gameCount}</p>
+        <p class="main-score-display">Games tied: ${gamesTied}</p>`;
   }
-  // Computer selection comparison
-  if (computerChoice === "rock" && playerChoice === "scissors") {
-    computerRoundWinner(playerChoice, computerChoice);
-    computerCount++;
-  } else if (computerChoice === "scissors" && playerChoice === "paper") {
-    computerRoundWinner(playerChoice, computerChoice);
-    computerCount++;
-  } else if (computerChoice === "paper" && playerChoice === "rock") {
-    computerRoundWinner(playerChoice, computerChoice);
-    computerCount++;
-  }
-  // Game counter
-  gameCounter++;
 
-  // Console Display
-  displayResults(
-    playerCount,
-    computerCount,
-    gamesDrawnCounter,
-    gameCounter,
-    playerChoice,
-    computerChoice
-  );
-  // Return winner
-  if (playerCount === 3) {
-    console.log(`Player Wins!`);
-    return 3;
-  } else if (computerCount === 3) {
-    console.log(`Computer Wins!`);
-    return 3;
+  // Computer selection
+  if (computerSelection === "rock" && playerSelection === "scissors") {
+    console.log(
+      `Player says: ${playerSelection}. Computer says: ${computerSelection}`
+    );
+    console.log(
+      `Computer wins this round ${computerSelection} beats ${playerSelection}`
+    );
+    gameCount++;
+    computerCount++;
+    mainScoreDisplay.innerHTML = `   <p class="main-score-round-winner">
+          Computer wins this round! <br />
+          ${computerSelection} beats ${playerSelection}
+        </p>
+        <p class="main-score-display">Games played: ${gameCount}</p>
+        <p class="main-score-display">Games tied: ${gamesTied}</p>`;
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    console.log(
+      `Player says: ${playerSelection}. Computer says: ${computerSelection}`
+    );
+    console.log(
+      `Computer wins this round ${computerSelection} beats ${playerSelection}`
+    );
+    gameCount++;
+    computerCount++;
+    mainScoreDisplay.innerHTML = `   <p class="main-score-round-winner">
+          Computer wins this round! <br />
+          ${computerSelection} beats ${playerSelection}
+        </p>
+        <p class="main-score-display">Games played: ${gameCount}</p>
+        <p class="main-score-display">Games tied: ${gamesTied}</p>`;
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    console.log(
+      `Player says: ${playerSelection}. Computer says: ${computerSelection}`
+    );
+    console.log(
+      `Computer wins this round ${computerSelection} beats ${playerSelection}`
+    );
+    gameCount++;
+    computerCount++;
+    mainScoreDisplay.innerHTML = `   <p class="main-score-round-winner">
+          Computer wins this round! <br />
+          ${computerSelection} beats ${playerSelection}
+        </p>
+        <p class="main-score-display">Games played: ${gameCount}</p>
+        <p class="main-score-display">Games tied: ${gamesTied}</p>`;
+  }
+  // gameCount++;
+  console.log(`Games played: ${gameCount}`);
+  console.log(`Games tied: ${gamesTied}`);
+  console.log(`Player games won: ${playerCount}`);
+  console.log(`Computer games won: ${computerCount}`);
+  // console.log(`Games tied: ${gamesTied}`);
+  playerScore.innerHTML = `${playerCount}`;
+  computerScore.innerHTML = `${computerCount}`;
+
+  if (playerCount === 5) {
+    return 5;
+  } else if (computerCount === 5) {
+    return 5;
   }
 }
 
-// Start game
 // game(playerSelection, computerPlay());
+// playRound(playerSelection, computerPlay());
 
-// Write game() function that calls the playRound() function
-// and play a 5-round game that keeps score and declares a
-// winner, or loser
 function game(playerSelection, computerSelection) {
-  // Internal game counter to monitor games played
-  let internalGameCounter = 7;
-  for (i = 0; i < internalGameCounter; i++) {
-    playerSelection = prompt("Enter a selection");
-    if (playRound(playerSelection, computerSelection) === 3) {
-      break;
-    } else {
-      internalGameCounter++;
-    }
-  }
+  // for (let i = 0; i < 17; i++) {
+  // playerSelection = prompt("Emter a choice...");
+  playRound(playerSelection, computerPlay());
+  // }
 }
 
-// Display results
-function displayResults(
-  playerCount,
-  computerCount,
-  gamesDrawnCounter,
-  gameCounter,
-  playerChoice,
-  computerChoice
-) {
-  console.log(
-    `Player plays "${playerChoice.toUpperCase()}", Computer plays "${computerChoice.toUpperCase()}"`
-  );
-
-  console.log(
-    `Games played: ${gameCounter} | Games tied: ${gamesDrawnCounter}`
-  );
-  console.log("Games Won:");
-  console.log(`Player: ${playerCount}`);
-  console.log(`Computer: ${computerCount}`);
-  console.log(`----------------------------------------------`);
-  console.log(`----------------------------------------------`);
+function displayGame(playerSelection, computerSelection, gameCount, gamesTied) {
+  mainScoreDisplay.innerHTML = `   <p class="main-score-round-winner">
+          You win this round! <br />
+          ${playerSelection} beats ${computerSelection}
+        </p>
+        <p class="main-score-display">Games played: ${gameCount}</p>
+        <p class="main-score-display">Games tied: ${gamesTied}</p>`;
 }
 
-// Declare winners
-function playerRoundWinner(playerChoice, computerChoice, gameDisplay) {
-  resultsDisplay.textContent = `You Win! ${capitalizePlayer(
-    playerChoice
-  )} beats ${capitalizeComputer(computerChoice)}`;
-  return console.log(
-    `You Win! ${capitalizePlayer(playerChoice)} beats ${capitalizeComputer(
-      computerChoice
-    )}`
-  );
-}
-
-function computerRoundWinner(playerChoice, computerChoice) {
-  resultsDisplay.textContent = `Computer wins this round! ${capitalizeComputer(
-    computerChoice
-  )} beats ${capitalizePlayer(playerChoice)}`;
-  console.log(
-    `Computer wins this round! ${capitalizeComputer(
-      computerChoice
-    )} beats ${capitalizePlayer(playerChoice)}`
-  );
-}
-
-// Capitalize first letter for display
-function capitalizePlayer(playerChoice) {
-  firstLetter = playerChoice.charAt(0).toUpperCase();
-  let result = playerChoice.replace(playerChoice.charAt(0), firstLetter);
-  return result;
-}
-function capitalizeComputer(computerChoice) {
-  firstLetter = computerChoice.charAt(0).toUpperCase();
-  let result = computerChoice.replace(computerChoice.charAt(0), firstLetter);
-  return result;
+function sameChoice(gameCount, gamesTied) {
+  mainScoreDisplay.innerHTML = `   <p class="main-score-round-winner">
+          You chose the same<br />
+          Try again!
+        </p>
+        <p class="main-score-display">Games Played: ${gameCount}</p>
+        <p class="main-score-display">Games tied: ${gamesTied}</p>`;
 }
